ea5b2d69c57c18cd762c9efe60b7145b87b4a971
vitex <vitex@exiv.jinonice.czf>
2022-05-31 22:09:21 +0200

    Initial implementation of Changes processing classes

---
+++ src/AbraFlexi/Bricks/Change.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * AbraFlexi Bricks - One Change Class
+ *
+ * @author     Vítězslav Dvořák <info@vitexsofware.cz>
+ * @copyright  (G) 2022 Vitex Software
+ */
+
+namespace AbraFlexi\Bricks;
+
+/**
+ * Handle one AbraFlexi change record
+ *
+ * @author vitex
+ */
+class Change {
+
+    /**
+     * Evidence of record
+     * @var string
+     */
+    public $evidence = null; // "faktura-vydana",
+
+    /**
+     * Change IN Version
+     * @var int
+     */
+    public $inVersion = null; // "3",
+
+    /**
+     * create, update, delete
+     * @var string
+     */
+    public $operation = null; //"create",
+
+    /**
+     * 
+     * @var \AbraFlexi\DateTime
+     */
+    public $timestamp = null; // "2019-01-01 00:00:00.0",
+
+    /**
+     * 
+     * @var integer
+     */
+    public $id = null; // "1",
+
+    /**
+     * Ext-IDs for record
+     * @var array
+     */
+    public $extIds = []; // []
+
+    /**
+     * One AbraFlexi change
+     * 
+     * @param array $changeData
+     */
+    public function __construct($changeData = []) {
+        if ($changeData) {
+            $this->setData($changeData);
+        }
+    }
+
+    /**
+     * Give your data back
+     * 
+     * @return array
+     */
+    public function getData() {
+        return [
+            "@evidence" => $this->evidence,
+            "@in-version" => $this->inVersion,
+            "@operation" => $this->operation,
+            "@timestamp" => $this->timestamp,
+            "id" => $this->id,
+            "external-ids" => $this->extIds
+        ];
+    }
+
+    /**
+     * Store data
+     * @param array $changeData
+     */
+    public function setData($changeData) {
+        $this->evidence = $changeData['@evidence'];
+        $this->inVersion = $changeData['@in-version'];
+        $this->operation = $changeData['@operation'];
+        $this->timestamp = $changeData['@timestamp'];
+        $this->id = $changeData['id'];
+        $this->id = $changeData['external-ids'];
+    }
+
+}
---
+++ src/AbraFlexi/Bricks/Changes.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * AbraFlexi Bricks - Changes Class
+ *
+ * @author     Vítězslav Dvořák <info@vitexsofware.cz>
+ * @copyright  (G) 2022 Vitex Software
+ */
+
+namespace AbraFlexi\Bricks;
+
+/**
+ * WebHook data Handler class
+ * 
+ * @see https://podpora.flexibee.eu/cs/articles/4744362-changes-api
+ *
+ * @author vitex
+ */
+class Changes {
+
+    /**
+     * Reported number of last change 
+     * @var int
+     */
+    public $globalVersion = null;
+
+    /**
+     * Stored Changes
+     * @var array<Change>
+     */
+    public $changes = [];
+
+    /**
+     * Next chunk ID
+     * @var int
+     */
+    public $next = null;
+
+    /**
+     *  
+     * @param array $changesData
+     */
+    public function __construct($changesData) {
+        if (array_key_exists('changes', $changesData)) {
+            foreach ($changesData['changes'] as $changeData) {
+                $this->changes[] = new Change($changeData);
+            }
+        }
+        if (array_key_exists('@globalVersion', $changesData)) {
+            $this->globalVersion = intval($changeData['@globalVersion']);
+        }
+        if (array_key_exists('next', $changesData)) {
+            $this->next = intval($changeData['next']);
+        }
+    }
+
+    /**
+     * Show recursive difference between two multidimensional arrays
+     * 
+     * @param array $data
+     * @param array $datb
+     */
+    public static function dataDiff($data, $datb) {
+        $diff = [];
+        $columns = array_merge(array_combine(array_keys($data), array_keys($data)), array_combine(array_keys($datb), array_keys($datb)));
+        foreach ($columns as $column) {
+            if ((array_key_exists($column, $data) && is_array($data[$column])) || (array_key_exists($column, $datb) && is_array($datb[$column]))) {
+                $diff[$column] = self::dataDiff(array_key_exists($column, $data) ? $data[$column] : [], array_key_exists($column, $datb) ? $datb[$column] : []);
+            } else {
+                if (array_key_exists($column, $data) && array_key_exists($column, $datb) && $data[$column] == $datb[$column]) {
+                    unset($diff[$column]);
+                }
+            }
+        }
+        return $diff;
+    }
+
+}
